5d4730fad828dfce68295153690e4c6edd319d06
maksimTemp <mr.maksim.rudenko.2002@mail.ru>
2023-04-08 12:45:43 +0700

    feat: Add Controllers and minor changes

--- DeliveryAPI/Controllers/DeliveryController.cs
+++ DeliveryAPI/Controllers/DeliveryController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DeliveryAPI.Domain;
+using DeliveryAPI.Models.Requests;
+using DeliveryAPI.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DeliveryAPI.Controllers
@@ -7,5 +10,41 @@ namespace DeliveryAPI.Controllers
     [ApiController]
     public class DeliveryController : ControllerBase
     {
+        private readonly IDeliveryService _service;
+
+        public DeliveryController(IDeliveryService orderService)
+        {
+            _service = orderService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Delivery> Get(Guid id)
+        {
+            return await _service.GetByIdAsync(id);
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Delivery>> GetAll()
+        {
+            return await _service.GetAll();
+        }
+
+        [HttpPost]
+        public async Task<Delivery> Create([FromBody] CreateDeliveryRequest req)
+        {
+            return await _service.CreateAsync(req);
+        }
+
+        [HttpPut]
+        public async Task<Delivery> Update([FromBody] Delivery req)
+        {
+            return await _service.UpdateAsync(req);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<int> Delete(Guid id)
+        {
+            return await _service.DeleteAsync(id);
+        }
     }
 }
---
+++ DeliveryAPI/Models/Requests/CreateDeliveryRequest.cs
@@ -0,0 +1,17 @@
+﻿using DeliveryAPI.Domain;
+using SharedLibrary.Enums;
+
+namespace DeliveryAPI.Models.Requests
+{
+    public class CreateDeliveryRequest
+    {
+        public DeliveryStatuses Status { get; set; }
+        public Guid CourierId { get; set; }
+        public string CourierName { get; set; }
+        public Guid CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string? Adress { get; set; }
+        public decimal TotalPrice { get; set; }
+        public IEnumerable<DeliveryItem> Items { get; set; }
+    }
+}
